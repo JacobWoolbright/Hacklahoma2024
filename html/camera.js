4a470d60592741776fb348a3d99952c0aa744dbc
JLW1808 <jacob.woolbright1234@gmail.com>
2024-02-10 16:23:21 -0600

    add starting code for pose detection

---
+++ html/camera.js
@@ -0,0 +1,26 @@
+console.log("reading pose");
+const imageScaleFactor = 0.50;
+const flipHorizontal = false;
+const outputStride = 16;
+// get up to 5 poses
+const maxPoseDetections = 2;
+// minimum confidence of the root part of a pose
+const scoreThreshold = 0.5;
+// minimum distance in pixels between the root parts of poses
+const nmsRadius = 20;
+
+var net;
+
+
+async function loadModel(){
+    net = await posenet.load();
+}
+
+async function readPose(imageElement) {
+
+    const poses = await net.estimateMultiplePoses(
+        imageElement, imageScaleFactor, flipHorizontal, outputStride,
+        maxPoseDetections, scoreThreshold, nmsRadius);
+    console.log("estimating pose done");
+    console.log(poses);
+}
