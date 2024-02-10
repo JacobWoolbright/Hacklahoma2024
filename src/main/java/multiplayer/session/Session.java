a74d2ac569a14cb75c9d80ea752161a85f75da9d
JLW1808 <jacob.woolbright1234@gmail.com>
2024-02-10 17:36:13 -0600

    initial session/session manager code

--- src/main/java/multiplayer/session/Session.java
+++ src/main/java/multiplayer/session/Session.java
@@ -1,6 +1,16 @@
 package multiplayer.session;
 
+import utils.IdGenerator;
+
+import java.util.UUID;
+
 public class Session {
+
+    final UUID id;
+
+    public Session() {
+        id = IdGenerator.generateId();
+    }
 
 
 
--- src/main/java/multiplayer/session/SessionManager.java
+++ src/main/java/multiplayer/session/SessionManager.java
@@ -2,6 +2,14 @@ package multiplayer.session;
 
 public class SessionManager {
 
+    private static SessionManager instance = new SessionManager();
 
+    private SessionManager() {}
 
+    public static SessionManager getInstance() {
+        if (instance == null){
+            instance = new SessionManager();
+        }
+        return instance;
+    }
 }
