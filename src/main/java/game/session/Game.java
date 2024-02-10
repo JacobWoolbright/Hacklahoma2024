3a4ef9d958d5c1df9cdf4145d15af33de39dc455
JLW1808 <jacob.woolbright1234@gmail.com>
2024-02-10 17:55:47 -0600

    add sharecode into game

--- src/main/java/game/session/Game.java
+++ src/main/java/game/session/Game.java
@@ -1,4 +1,15 @@
 package game.session;
 
+import utils.ShareCodeGenerator;
+
 public class Game {
+
+    public String gameShareCode;
+
+    public String getGameShareCode() {
+        if(gameShareCode == null) {
+            gameShareCode = ShareCodeGenerator.generateShareCode();
+        }
+        return gameShareCode;
+    }
 }
