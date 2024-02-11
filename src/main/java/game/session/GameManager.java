2740c7a9a0f258c70c48361615998243f863d937
JLW1808 <jacob.woolbright1234@gmail.com>
2024-02-10 18:32:55 -0600

    GameManager uses HashMap rather then list

--- src/main/java/game/session/GameManager.java
+++ src/main/java/game/session/GameManager.java
@@ -1,11 +1,14 @@
 package game.session;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class GameManager {
-    private static ArrayList<Game> games = new ArrayList<>();
+    private static HashMap<UUID, Game> games = new HashMap<>();
 
     public static void addGame(Game game) {
-        games.add(game);
+        games.put(game.getGameId(), game);
     }
 }
