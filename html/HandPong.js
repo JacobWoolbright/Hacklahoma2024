3f3e392a1da4be36191e175447ef28c601c0e951
Aaron Massey <03masseyaaron@gmail.com>
2024-02-10 23:43:09 -0600

    game ready for reading data

--- html/HandPong.js
+++ html/HandPong.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
-const roundsToWin = 7;
+const roundsToWin = 100;
 
 class player {
     get name() {
@@ -46,8 +46,8 @@ class player {
     constructor(name, x, y, width, height, wins, integral, prevError, primary, relativeY) {
         this.width = width;
         this.height = height;
-        this.integral = integral;
-        this.prevError = prevError;
+        this.integral = 0;
+        this.prevError = 0;
         this._name = name;
         this.x = x;
         this._y = y;
@@ -61,19 +61,22 @@ class player {
 
 player1 = new player("Player 1", .0125 * canvasWidth, canvasHeight / 2 - 100, 0.015 * canvasHeight, 0.15 * canvasHeight, 0, 0, 0, true, 0);
 player2 = new player("Player 2", canvasWidth - .0125 * canvasWidth, canvasHeight / 2 - 100, 0.015 * canvasHeight, 0.15 * canvasHeight, 0, 0, 0, false, 0);
-// Ball
+
+
 let ball = {
     x: canvasWidth / 2,
     y: canvasHeight / 2,
     radius: canvasWidth / 100,
-    dx: 2 / canvasWidth,
-    dy: 2 / canvasHeight
+    dx: 1/800 * canvasWidth,
+    dy: 1/600 * canvasHeight
 
 };
 
 function reset(){
-    player1.score = 0;
-    player2.score = 0;
+    player1.wins = 0;
+    player2.wins = 0;
+    player1.y = canvasHeight / 2 - 100;
+    player2.y = canvasHeight / 2 - 100;
 
 }
 
@@ -95,10 +98,97 @@ function updatePlayer(location, y, player) {
     return Kp * error + Ki * player.integral + Kd * derivative;
 }
 
+function drawScore()  {
+    ctx.font = "50px Arial";
+    ctx.fillText(player1.wins, canvasWidth / 4, 50);
+    ctx.fillText(player2.wins, canvasWidth * 3 / 4, 50);
+}
 
-function gameLoop() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+function drawNames() {
+    ctx.font = "30px Arial";
+    ctx.fillText(player1.name, 50, 50);
+    ctx.fillText(player2.name, canvasWidth - 200, 50);
 
+}
+
+function drawCenterLine() {
+    ctx.beginPath();
+    ctx.setLineDash([canvasHeight / 20]);
+    ctx.moveTo(canvasWidth / 2, 0);
+    ctx.lineTo(canvasWidth / 2, canvasHeight);
+    ctx.strokeStyle = "#FFF";
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.rect(ball.x, ball.y, ball.radius, ball.radius);
+    ctx.fillStyle = "#FFF";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPlayers() {
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(player1.x, player1.y, player1.width, player1.height);
+    ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
+}
+
+function testGoal() {
+    //if the ball goes off the screen to the right, player 1 scores
+    if (ball.x + ball.radius > canvasWidth) {
+        ball.x = canvasWidth / 2;
+        ball.y = canvasHeight / 2;
+        ball.dx = -ball.dx;
+        player1.wins++;
+    }
+    //if the ball goes off the screen to the left, player 2 scores
+    if (ball.x - ball.radius < 0) {
+        ball.x = canvasWidth / 2;
+        ball.y = canvasHeight / 2;
+        ball.dx = -ball.dx;
+        player2.wins++;
+    }
+}
+
+function testWin() {
+    if (player1.wins === roundsToWin) {
+        alert("Player 1 wins!");
+        reset();
+    }
+
+    if (player2.wins === roundsToWin) {
+        alert("Player 2 wins!");
+        reset();
+    }
+}
+
+function testPlayerCollide() {
+    if (ball.x - ball.radius <= player1.x + player1.width &&
+        ball.y >= player1.y &&
+        ball.y <= player1.y + player1.height) {
+        ball.dx = -ball.dx;
+    }
+    if (ball.x + ball.radius >= player2.x &&
+        ball.y >= player2.y &&
+        ball.y <= player2.y + player2.width) {
+        ball.dx = -ball.dx;
+    }
+}
+
+function testWallCollide() {
+    if (ball.y + ball.radius >= canvasHeight || ball.y - ball.radius <= 0) {
+        ball.dy = -ball.dy;
+    }
+}
+
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+function movePlayers() {
     // Update the paddle positions
     player1.y += updatePlayer(player1.relativeY, player1.y, player1);
     player2.y += updatePlayer(player2.relativeY, player2.y, player2);
@@ -115,83 +205,32 @@ function gameLoop() {
     } else if (player2.y + player2.height > canvasHeight) {
         player2.y = canvasHeight - player2.width;
     }
+}
 
-    // Collision detection with walls
-    if (ball.y + ball.radius >= canvasHeight || ball.y - ball.radius <= 0) {
-        ball.dy = -ball.dy;
-    }
+function gameLoop() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    // Collision detection with paddles
-    if (ball.x - ball.radius <= player1.x + player1.width &&
-        ball.y >= player1.y &&
-        ball.y <= player1.y + player1.height) {
-        ball.dx = -ball.dx;
-    }
-    if (ball.x + ball.radius >= player2.x &&
-        ball.y >= player2.y &&
-        ball.y <= player2.y + player2.width) {
-        ball.dx = -ball.dx;
-    }
+    //readData();
 
-    // Move the ball
-    ball.x += ball.dx;
-    ball.y += ball.dy;
+    moveBall();
+    movePlayers();
+    testWallCollide();
+    testPlayerCollide();
 
-    // Draw paddles
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(player1.x, player1.y, player1.width, player1.height);
-    ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
+    //sendBallData();
+    //sendPlayerData();
 
-    // Draw ball
-    ctx.beginPath();
-    ctx.rect(ball.x, ball.y, ball.radius, ball.radius);
-    ctx.fillStyle = "#FFF";
-    ctx.fill();
-    ctx.closePath();
 
-    // Draw the score
-    ctx.font = "30px Arial";
-    ctx.fillText(player1.wins, canvasWidth / 2 - 50, canvasHeight - 50);
-    ctx.fillText(player2.wins, canvasWidth / 2 + 20, canvasHeight - 50);
+    // Draw the game objects
+    drawPlayers();
+    drawBall();
+    drawScore();
+    drawNames();
+    drawCenterLine();
+    testGoal();
+    testWin();
 
-    // Draw the center line
-    ctx.beginPath();
-    ctx.setLineDash([10, 10]);
-    ctx.moveTo(canvasWidth / 2, 0);
-    ctx.lineTo(canvasWidth / 2, canvasHeight);
-    ctx.strokeStyle = "#FFF";
-    ctx.stroke();
-    ctx.closePath();
-
-    // Draw the names
-    ctx.font = "30px Arial";
-    ctx.fillText(player1.name, 50, 50);
-    ctx.fillText(player2.name, canvasWidth - 200, 50);
-
-    //if the ball goes off the screen to the right, player 1 scores
-    if (ball.x + ball.radius > canvasWidth) {
-        ball.x = canvasWidth / 2;
-        ball.y = canvasHeight / 2;
-        ball.dx = -ball.dx;
-        player1.wins++;
-    }
-    //if the ball goes off the screen to the left, player 2 scores
-    if (ball.x - ball.radius < 0) {
-        ball.x = canvasWidth / 2;
-        ball.y = canvasHeight / 2;
-        ball.dx = -ball.dx;
-        player2.wins++;
-    }
-
-    if (player1.wins === roundsToWin) {
-        alert("Player 1 wins!");
-        reset();
-    }
-
-    if (player2.wins === roundsToWin) {
-        alert("Player 2 wins!");
-        reset();
-    }
 
     // Call the gameLoop function again
     requestAnimationFrame(gameLoop);
