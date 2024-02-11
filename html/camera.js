e106e7587320f231984463e7b826b524e1e69ddb
JLW1808 <jacob.woolbright1234@gmail.com>
2024-02-11 01:26:00 -0600

    add scaling logic

--- html/camera.js
+++ html/camera.js
@@ -22,7 +22,19 @@ async function readPose(imageElement) {
         imageElement, imageScaleFactor, flipHorizontal, outputStride,
         maxPoseDetections, scoreThreshold, nmsRadius);
 
-    pose = poses[0];
+    rightWrist = poses[0].keypoints[10].position["y"];
+    rightSholder = poses[0].keypoints[6].position["y"];
+    leftWrist = poses[0].keypoints[9].position["y"];
+    leftSholder = poses[0].keypoints[5].position["y"];
+    leftEye = poses[0].keypoints[1].position["y"];
+    rightEye = poses[0].keypoints[2].position["y"];
 
-    console.log(pose)
+    sholderHeight = (rightSholder + leftSholder) / 2;
+
+    upperBound = (leftEye + rightEye) / 2;
+    lowerBound = upperBound + (sholderHeight-upperBound)*2.8;
+
+    ratio = (rightWrist - lowerBound) / (upperBound - lowerBound);
+
+    console.log(ratio);
 }
